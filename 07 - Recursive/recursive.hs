d1e0476ad625116f121c11413668de1c7c88a7d7
HuQingyang <i@do.codes>
2017-12-17 00:30:22 +0800

    Recursive

--- 07 - Recursive/recursive.hs
+++ 07 - Recursive/recursive.hs
@@ -18,10 +18,37 @@ reverse' :: [a] -> [a]
 reverse' [] = []
 reverse' (x:xs) = reverse' xs ++ [x]
 
+repeat' :: a -> [a]
+repeat' x = x:repeat' x
+
+zip' :: [a] -> [b] -> [(a, b)]
+zip' _ [] = []
+zip' [] _ = []
+zip' (x:xs) (y:ys) = (x, y):zip' xs ys
+
+elem' :: (Eq a) => a -> [a] -> Bool
+a `elem'` [] = False
+a `elem'` (x:xs)
+    | a == x = True
+    | otherwise = a `elem'` xs
+
+quickSort :: (Ord a) => [a] -> [a]
+quickSort [] = []
+quickSort (x:xs) =
+    let smallerOrEqual = [a | a <- xs, a <= x]
+        larger = [a | a <- xs, a > x]
+    in quickSort smallerOrEqual ++ [x] ++ quickSort larger
+
+
 
 main = do
-    print (maximum' [1, 2, 3, 4])   -- 4
-    print (maximum' "Aloha!")       -- o
-    print (replicate' 3 5)          -- [5, 5, 5]
-    print (take 2 [3, 4, 5])        -- [3, 4]
-    print (reverse' "Hello!")       -- "!olleH"
+    print (maximum' [1, 2, 3, 4])       -- 4
+    print (maximum' "Aloha!")           -- o
+    print (replicate' 3 5)              -- [5, 5, 5]
+    print (take 2 [3, 4, 5])            -- [3, 4]
+    print (reverse' "Hello!")           -- "!olleH"
+    print (take' 3 (repeat' 2))         -- [2, 2, 2]
+    print (zip' [1..4] "abc")           -- [(1, a), (2, b), (3, c)]
+    print ('a' `elem'` "Happy")         -- True
+    print (quickSort [9, 2, 7, 1, 5])   -- [1, 2, 5, 7, 9]
+
