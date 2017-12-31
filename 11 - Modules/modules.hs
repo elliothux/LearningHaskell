9f34bd9d5e9e83c5736f248c7b9e0b610eb0a286
胡青杨 <i@do.codes>
2018-01-01 02:09:40 +0800

    Modules

---
+++ 11 - Modules/modules.hs
@@ -0,0 +1,16 @@
+
+-- 导入模块
+-- import Data.List
+
+-- 导入模块中的函数
+-- import Data.List (nub, sort)
+
+-- 导入除了某个函数之外的其他函数
+-- import Data.List hiding (nub)
+
+-- 除了 hiding, 还可以使用 qualified import 避免命名冲突
+import qualified Data.Map as M
+
+
+main = do
+    print "Hello"
